32c23543b3976acbdbd80903e4f2b48d5eeea8be
GABRIEL MAKOWER <gabrielm1571@hstat.org>
2018-03-12 15:32:09 +0000

    done and working

--- application_controller.rb
+++ application_controller.rb
@@ -9,5 +9,15 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
-  
+  post '/result' do
+    #puts params 
+    fav_num= params[:fav_num]
+    @num= get_fav_num(fav_num)
+    date= params[:date]
+    @date= get_date(date)
+    year= params[:year]
+   @year=  get_year(year)
+   erb :result 
+  end 
+
 end
--- models/model.rb
+++ models/model.rb
@@ -11,25 +11,40 @@ require 'json'
 #octocat.keys
 # ["login", "id", "avatar_url", "gravatar_id", "url", "html_url", "followers_url", "following_url", "gists_url", "starred_url", "subscriptions_url", "organizations_url", "repos_url", "events_url", "received_events_url", "type", "name", "company", "blog", "location", "email", "hireable", "bio", "public_repos", "followers", "following", "created_at", "updated_at", "public_gists"]
 
+#def get_fav_num(fav_num)
+    #uri = URI('http://numbersapi.com/13/math')
+#fav_num = Net::HTTP.get(uri)
+#puts fav_num
+#end
+##puts get_fav_num("14")
+
+
 def get_fav_num(fav_num)
-    uri = URI('http://numbersapi.com/13/math')
-fav_num = Net::HTTP.get(uri)
-puts fav_num
+    uri = URI("http://numbersapi.com/#{fav_num}/math")
+num_fact = Net::HTTP.get(uri)
+ num_fact
 end 
-puts get_fav_num("14")
+#fav_num= gets.chomp
+#puts get_fav_num(fav_num)
+
 
 
 def get_date(date)
-    uri = URI('http://numbersapi.com/08/22/date')
-date = Net::HTTP.get(uri)
-puts date
+    uri = URI("http://numbersapi.com/#{date}/date")
+ date_fact= Net::HTTP.get(uri)
+    date_fact
 end 
-puts get_date("14")
+#date= gets.chomp
+#puts get_date("08/22")
+
+
+
 
 def get_year(year)
-    uri = URI('http://numbersapi.com/2000/year')
-year = Net::HTTP.get(uri)
-puts year
+    uri = URI("http://numbersapi.com/#{year}/year")
+year_fact = Net::HTTP.get(uri)
+ year_fact
 end 
-puts get_year("14")
+#year= gets.chomp
+#puts get_year("2000")
 
