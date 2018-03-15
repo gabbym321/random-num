311afca6d34c712ff08c7e64b795fc69566ec7fd
GABRIEL MAKOWER <gabrielm1571@hstat.org>
2018-03-15 16:08:07 +0000

    add comments

--- application_controller.rb
+++ application_controller.rb
@@ -6,12 +6,12 @@ require_relative 'models/model.rb'
 
 class ApplicationController < Sinatra::Base
 
-  get '/' do
+  get '/' do #this gets the route from the index (the forms)
     erb :index
   end
-  post '/result' do
-    #puts params 
-    fav_num= params[:fav_num]
+  post '/result' do #This says put the route we got from index and post it in the results page. 
+    
+    fav_num= params[:fav_num] #puts out the data we got from the fav_num form (same for line 16 & 18 with different info)
     @num= get_fav_num(fav_num)
     date= params[:date]
     @date= get_date(date)
--- models/model.rb
+++ models/model.rb
@@ -1,31 +1,15 @@
-#http://numbersapi.com/17/math
+
  
 require 'net/http'
 require 'json'
-#require 'pp'
-
-#uri = URI('http://numbersapi.com/13/math')
-#response = Net::HTTP.get(uri)
-#puts response
-
-#octocat.keys
-# ["login", "id", "avatar_url", "gravatar_id", "url", "html_url", "followers_url", "following_url", "gists_url", "starred_url", "subscriptions_url", "organizations_url", "repos_url", "events_url", "received_events_url", "type", "name", "company", "blog", "location", "email", "hireable", "bio", "public_repos", "followers", "following", "created_at", "updated_at", "public_gists"]
-
-#def get_fav_num(fav_num)
-    #uri = URI('http://numbersapi.com/13/math')
-#fav_num = Net::HTTP.get(uri)
-#puts fav_num
-#end
-##puts get_fav_num("14")
 
 
-def get_fav_num(fav_num)
-    uri = URI("http://numbersapi.com/#{fav_num}/math")
-num_fact = Net::HTTP.get(uri)
- num_fact
+def get_fav_num(fav_num) 
+    uri = URI("http://numbersapi.com/#{fav_num}/math") #link to the api url. Used string interplatation so that it would work for any number you put in. same for lines 17 & 27
+num_fact = Net::HTTP.get(uri) #gets url same for line 18 & 28
+ num_fact #return the info got same for line 19 & 29
 end 
-#fav_num= gets.chomp
-#puts get_fav_num(fav_num)
+
 
 
 
@@ -34,8 +18,7 @@ def get_date(date)
  date_fact= Net::HTTP.get(uri)
     date_fact
 end 
-#date= gets.chomp
-#puts get_date("08/22")
+
 
 
 
@@ -45,6 +28,5 @@ def get_year(year)
 year_fact = Net::HTTP.get(uri)
  year_fact
 end 
-#year= gets.chomp
-#puts get_year("2000")
+
 
